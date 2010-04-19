4eb69e8d18b3a6ee62599b08350403533f536887
Tom Brow <tebrow@gmail.com>
2010-04-19 15:56:06 -0700

    correct PDF aspect scaling

--- Classes/PDFExampleViewController.m
+++ Classes/PDFExampleViewController.m
@@ -25,6 +25,15 @@
     [super dealloc];
 }
 
+- (CGAffineTransform) aspectFitRect:(CGRect)pageRect inRect:(CGRect)contextRect {
+	CGFloat scaleFactor = MIN(contextRect.size.width/pageRect.size.width, contextRect.size.width/pageRect.size.width);
+	CGAffineTransform translation = 
+		CGAffineTransformMakeTranslation((contextRect.size.width - pageRect.size.width * scaleFactor) / 2, 
+										 (contextRect.size.height - pageRect.size.height * scaleFactor) / 2);
+	CGAffineTransform scale = CGAffineTransformMakeScale(scaleFactor, scaleFactor);
+	return CGAffineTransformConcat(scale, translation);
+}
+
 #pragma mark LeavesViewDataSource methods
 
 - (NSUInteger) numberOfPagesInLeavesView:(LeavesView*)leavesView {
@@ -33,11 +42,9 @@
 
 - (void) renderPageAtIndex:(NSUInteger)index inContext:(CGContextRef)ctx {
 	CGPDFPageRef page = CGPDFDocumentGetPage(pdf, index + 1);
-	CGAffineTransform transform = CGPDFPageGetDrawingTransform(page, 
-															   kCGPDFMediaBox, 
-															   CGContextGetClipBoundingBox(ctx), 
-															   0, 
-															   true);
+	CGAffineTransform transform = [self aspectFitRect:CGPDFPageGetBoxRect(page, kCGPDFMediaBox) 
+											   inRect:CGContextGetClipBoundingBox(ctx)
+								   ];
 	CGContextConcatCTM(ctx, transform);
 	CGContextDrawPDFPage(ctx, page);
 }
