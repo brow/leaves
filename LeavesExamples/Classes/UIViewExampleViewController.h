e558d2dfe6f7f505b3f5031a4fe1ea2533a31e28
Boy van der Laak <boyvanderlaak@gmail.com>
2014-01-23 13:49:14 +0100

    Added example for rendering page from UIView

---
+++ LeavesExamples/Classes/UIViewExampleViewController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewExampleViewController.h
+//  LeavesExamples
+//
+//  Created by Aemotion on 23-01-14.
+//  Copyright (c) 2014 Tom Brow. All rights reserved.
+//
+
+#import "LeavesViewController.h"
+
+@interface UIViewExampleViewController : LeavesViewController
+
+@end
